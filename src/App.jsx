a7104d0d7b7c3f7763c58eb789a4d61dccdd7982
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2025-04-30 16:14:10 -0400

    added navbar and mobile navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { LoadingScreen } from "./components/LoadingScreen"
 import Navbar from "./components/Navbar"
 
 import './index.css'
+import MobileMenu from "./components/MobileMenu"
 function App() {
   const [isLoaded, setIsLoaded] = useState(false)
   const [menuOpen, setMenuOpen] = useState(null)
@@ -10,7 +11,8 @@ function App() {
     <>
       {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
       <main className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
-        <Navbar />
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       </main>
       {/* <LoadingScreen /> */}
     </>
