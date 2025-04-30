dcceb640e446264e3f7cd2b941cabe059eb36ff6
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2025-04-30 15:43:40 -0400

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react"
 import { LoadingScreen } from "./components/LoadingScreen"
+import Navbar from "./components/Navbar"
+
 import './index.css'
 function App() {
   const [isLoaded, setIsLoaded] = useState(false)
@@ -7,7 +9,9 @@ function App() {
   return (
     <>
       {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
-      <main></main>
+      <main className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
+        <Navbar />
+      </main>
       {/* <LoadingScreen /> */}
     </>
   )
