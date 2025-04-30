f8f48c5b20166031a5fb69f1df0f419af20a732d
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2025-04-30 16:34:34 -0400

    added home page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar"
 
 import './index.css'
 import MobileMenu from "./components/MobileMenu"
+import Home from "./components/sections/Home"
 function App() {
   const [isLoaded, setIsLoaded] = useState(false)
   const [menuOpen, setMenuOpen] = useState(null)
@@ -13,6 +14,7 @@ function App() {
       <main className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <Home />
       </main>
       {/* <LoadingScreen /> */}
     </>
